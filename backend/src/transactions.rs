8d1bef4ad180fe4c12b9917fc917ae5aed0e480a
ChrisWhealy <chris@whealy.com>
2025-02-14 16:17:47 +0000

    Skip unsigned transactions

--- backend/src/transactions.rs
+++ backend/src/transactions.rs
@@ -12,7 +12,7 @@ pub fn process_transaction(
     let mut signed_txns: Vec<SignedUsdcTransaction> = Vec::new();
 
     match &txn.transaction {
-        // If present, extract instructions from parsed JSON transaction
+        // If present, extract instructions from signed, parsed JSON transaction
         EncodedTransaction::Json(
             UiTransaction {
                 message: UiMessage::Parsed(UiParsedMessage { instructions, .. }),
@@ -20,7 +20,7 @@ pub fn process_transaction(
                 ..
             },
             ..,
-        ) => instructions.iter().for_each(|i| {
+        ) if !signatures.is_empty() => instructions.iter().for_each(|i| {
             if let Some(txn) = process_instruction(i) {
                 info!("{}", txn);
                 signed_txns.push(SignedUsdcTransaction {
